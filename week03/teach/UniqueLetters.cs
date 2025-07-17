e803c4a1a70880bedb40a1746846e0606ff4b3f2
Your Name <your@email.com>
2025-07-17 15:28:57 -0700

    raise

--- week03/teach/UniqueLetters.cs
+++ week03/teach/UniqueLetters.cs
@@ -1,5 +1,9 @@
-﻿public static class UniqueLetters {
-    public static void Run() {
+﻿using System.Collections.Generic;
+
+public static class UniqueLetters
+{
+    public static void Run()
+    {
         var test1 = "abcdefghjiklmnopqrstuvwxyz"; // Expect True because all letters unique
         Console.WriteLine(AreUniqueLetters(test1));
 
@@ -13,16 +17,21 @@
     /// <summary>Determine if there are any duplicate letters in the text provided</summary>
     /// <param name="text">Text to check for duplicate letters</param>
     /// <returns>true if all letters are unique, otherwise false</returns>
-    private static bool AreUniqueLetters(string text) {
-        // TODO Problem 1 - Replace the O(n^2) algorithm to use sets and O(n) efficiency
-        for (var i = 0; i < text.Length; ++i) {
-            for (var j = 0; j < text.Length; ++j) {
-                // Don't want to compare to yourself ... that will always result in a match
-                if (i != j && text[i] == text[j])
-                    return false;
+    private static bool AreUniqueLetters(string text)
+    {
+        // A HashSet will only allow unique values to be entered.
+        var uniqueLetters = new HashSet<char>();
+        foreach (var letter in text)
+        {
+            // If we try to add a letter that is already in the set, the Add
+            // function will return false. This is how we know there is a duplicate.
+            if (!uniqueLetters.Add(letter))
+            {
+                return false;
             }
         }
 
+        // If we get here, then there are no duplicates.
         return true;
     }
 }
